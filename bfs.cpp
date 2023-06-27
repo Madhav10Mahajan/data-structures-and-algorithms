e736eb5d1496b5cdcd646f323c6bcb6ec6c2a29a
Madhav10Mahajan <92065736+Madhav10Mahajan@users.noreply.github.com>
2023-06-28 05:29:37 +0530

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,1 +1,16 @@
-
+void bfs(int start, unordered_map<int, vector<int>>&ourmap, unordered_map<int, bool>&visited) {
+	queue<int> q;
+	visited[start] = true;
+	q.push(start);
+	while (q.size() != 0) {
+		int front = q.front();
+		q.pop();
+		vector<int> temp = ourmap[front];
+		for (int i = 0; i < temp.size(); i++) {
+			if (visited[temp[i]] != true) {
+				visited[temp[i]] = true;
+				q.push(temp[i]);
+			}
+		}
+	}
+}
