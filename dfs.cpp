990d35cc608ebe736f981186d1ebadd872773da8
Madhav10Mahajan <92065736+Madhav10Mahajan@users.noreply.github.com>
2023-06-28 02:46:19 +0530

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -1,1 +1,10 @@
+void dfs(int start, unordered_map<int, vector<int>>&ourmap, unordered_map<int, bool>&visited) {
+	visited[start] = true;
+	vector<int> temp = ourmap[start];
+	for (int i = 0; i < temp.size(); i++) {
+		if (visited[temp[i]] != true) {
+			dfs(temp[i], ourmap, visited);
+		}
+	}
+}
 
