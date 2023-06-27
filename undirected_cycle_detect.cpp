52f9b82d7fdd111fc93cac812fb7062a8522d61e
Madhav10Mahajan <92065736+Madhav10Mahajan@users.noreply.github.com>
2023-06-28 03:34:44 +0530

    Update undirected_cycle_detect.cpp

--- undirected_cycle_detect.cpp
+++ undirected_cycle_detect.cpp
@@ -1,1 +1,20 @@
+bool iscycle(int start, unordered_map<int, vector<int>>&ourmap, unordered_map<int, bool>&visited, unordered_map<int, int> &parent, int &startnode, int&endnode) {
+	visited[start] = true;
+	vector<int> temp = ourmap[start];
+	for (int i = 0; i < temp.size(); i++) {
 
+		if (visited[temp[i]] == true && temp[i] != parent[start]) {
+			startnode = temp[i];
+			endnode = start;
+			return true;
+		}
+		else if (visited[temp[i]] != true) {
+			parent[temp[i]] = start;
+			bool ans = iscycle(temp[i], ourmap, visited, parent, startnode, endnode);
+			if (ans == true) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
