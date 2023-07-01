c50ae2425871afbd1752ec0e9adff8d743b431c0
Madhav10Mahajan <92065736+Madhav10Mahajan@users.noreply.github.com>
2023-07-02 03:52:53 +0530

    Update disjoint_set_union.cpp

--- disjoint_set_union.cpp
+++ disjoint_set_union.cpp
@@ -1,1 +1,35 @@
 
+int findparent(int node, vector<int>&parent) {
+
+	if (parent[node] == node) {
+		return node;
+	}
+	return parent[node] = findparent(parent[node], parent);
+}
+void unionbyrank(int u, int v, vector<int>&rank, vector<int>&parent) {
+
+	int pu = findparent(u, parent);
+	int pv = findparent(v, parent);
+	if (pu == pv) {
+		return;
+	}
+	if (rank[pu] > rank[pv]) {
+		parent[pv] = pu;
+	}
+	else if (rank[pv] > rank[pu]) {
+		parent[pu] = pv;
+	}
+	else {
+		parent[pv] = pu;
+		rank[pu]++;
+	}
+}
+void solve() {
+
+	vector<int> rank(n + 1, 0);
+	vector<int> parent(n + 1);
+	for (int i - 0; i < n + 1; i++) {
+		parent[i] = i;
+	}
+
+}
